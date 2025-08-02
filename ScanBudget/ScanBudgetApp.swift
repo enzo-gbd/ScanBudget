3959a178f5055df1269b3a77ab611635c51d677d
enzo-gbd <enzo.gouband@epitech.eu>
2025-08-02 17:49:45 +0200

    chore: bootstrap Xcode project

---
+++ ScanBudget/ScanBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanBudgetApp.swift
+//  ScanBudget
+//
+//  Created by Enzo Gouband on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScanBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScanBudgetTests/ScanBudgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScanBudgetTests.swift
+//  ScanBudgetTests
+//
+//  Created by Enzo Gouband on 02/08/2025.
+//
+
+import Testing
+@testable import ScanBudget
+
+struct ScanBudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
